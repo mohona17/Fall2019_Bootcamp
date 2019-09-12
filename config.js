646999bb098ed829b56a14727b58f2c58a054c95
mohona17 <mohonaahmed@ufl.edu>
2019-09-12 11:13:53 -0400

    Cleaned up config file

--- config.js
+++ config.js
@@ -5,9 +5,5 @@
 module.exports = {
     db: {
       uri: 'mongodb+srv://guest:6HFPgRwPdN0QzKQi@cluster0-za3ii.mongodb.net/test?retryWrites=true&w=majority',
-      //guest:6HFPgRwPdN0QzKQi
-      //uri: 'mongodb://root:password1@ds243963.mlab.com:43963/bootcamp-three',
-      //mongodb+srv://guest:VHJMnF6f3Oawe3DN@cen3031-course-za3ii.mongodb.net/test', //place the URI of your mongo database here.
-      //test:JLIvMbEYY4FC3sac
     }
   };
