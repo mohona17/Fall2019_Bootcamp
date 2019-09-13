2003d94f19e616ab12b0b020c72d65c1d58b26a1
mohona17 <mohonaahmed@ufl.edu>
2019-09-13 12:23:00 -0400

    completed JSONtoMongo.js file. properly pushes data in listings.json file to database

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -1,28 +1,32 @@
 'use strict';
 /* 
-  Import modules/files you may need to correctly run the script. 
-  Make sure to save your DB's uri in the config file, then import it with a require statement!
- */
+  Import modules/files you may need to correctly run the script.  */
+
 var fs = require('fs'),
-    mongoose = require('mongoose'), 
-    Schema = mongoose.Schema, 
-    Listing = require('./ListingSchema.js'), 
-    config = require('./config');
+  mongoose = require('mongoose'),
+  Schema = mongoose.Schema,
+  Listing = require('./ListingSchema.js'),  //returns model
+  listingData,
+  config = require('./config');// Make sure to save your DB's uri in the config file, then import it with a require statement!
 
 /* Connect to your database using mongoose - remember to keep your key secret*/
-//see https://mongoosejs.com/docs/connections.html
-//See https://docs.atlas.mongodb.com/driver-connection/
 
-/* 
-  Instantiate a mongoose model for each listing object in the JSON file, 
-  and then save it to your Mongo database 
-  //see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+mongoose.connect(config.db.uri, { useNewUrlParser: true ,useUnifiedTopology:true});
 
-  Remember that we needed to read in a file like we did in Bootcamp Assignment #1.
- */
+//Creating a document for DB. Listing is the model instantiated in ListingSchema.js
+Listing.createCollection(); 
 
+//Read File ------------------------------------------
+fs.readFile('listings.json', 'utf8', function(err, data) {
+  
+  if (err) throw err;
 
-/*  
-  Check to see if it works: Once you've written + run the script, check out your MongoLab database to ensure that 
-  it saved everything correctly. 
- */
+  //Parse JSON
+  listingData = data; //save the data in the listingData variable, 
+  let parseddata = JSON.parse(listingData);
+  console.log(parseddata);
+
+  //Saves the parsed data in the collection of the mongoose model instantiated before 
+  //Listing.collection.save(parseddata); this is deprecated
+  Listing.collection.insertOne(parseddata);
+}); 
