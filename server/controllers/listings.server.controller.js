16d169da13c455579b61bb5b97610009f52df8e6
mohona17 <mohonaahmed@ufl.edu>
2019-09-27 12:03:01 -0400

    listing all data test was having a problem since atlas DB somehow had too many files. going back to bootcamp2 to upload db again

--- server/controllers/listings.server.controller.js
+++ server/controllers/listings.server.controller.js
@@ -1,8 +1,9 @@
 /* Dependencies */
-var mongoose = require('mongoose'), 
-    Listing = require('../models/listings.server.model.js'),
-    coordinates = require('./coordinates.server.controller.js');
-    
+var mongoose = require('mongoose'),
+  Listing = require('../models/listings.server.model.js'),
+  coordinates = require('./coordinates.server.controller.js'),
+  app = require('../config/app.js');
+
 /*
   In this file, you should use Mongoose queries in order to retrieve/add/remove/update listings.
   On an error you should send a 404 status code, as well as the error message. 
@@ -19,95 +20,128 @@ var mongoose = require('mongoose'),
  */
 
 /* Create a listing */
-exports.create = function(req, res) {
+exports.create = function (req, res) {
 
   /* Instantiate a Listing */
   var listing = new Listing(req.body);
 
   /* save the coordinates (located in req.results if there is an address property) */
-  if(req.results) {
+  if (req.results) {
     listing.coordinates = {
-      latitude: req.results.lat, 
+      latitude: req.results.lat,
       longitude: req.results.lng
     };
   }
- 
+
   /* Then save the listing */
-  listing.save(function(err) {
-    if(err) {
-      console.log(err);
+  listing.save(function (err) {
+    if (err) {
+      //console.log(err);
       res.status(400).send(err);
     } else {
       res.json(listing);
-      console.log(listing)
+      //console.log(listing)
     }
   });
 };
+//----------------------------------------------------------------------------------------------
+
 
 /* Show the current listing */
-exports.read = function(req, res) {
+exports.read = function (req, res) {
   /* send back the listing as json from the request */
+  //console.log(req);
   res.json(req.listing);
 };
+//----------------------------------------------------------------------------------------------
 
 /* Update a listing - note the order in which this function is called by the router*/
 
-exports.update = function(req, res) {
+exports.update = function (req, res) {
   var listing = req.listing;
-  //DOES NOT WORK 
-  if(!listing.body) {
-    return res.status(400).send({
-        message: "Listing does not exist"
-    }); 
-  }
-  /* Replace the listings's properties with the new properties found in req.body */
-  Listing.findByIdAndUpdate(req.params.id, {
-    name: req.body.name, 
-    code: req.body.code    
-  });
-   /*save the coordinates (located in req.results if there is an address property) */
-  if(req.results) {
-    listing.coordinates = {
-      latitude: req.results.lat, 
-      longitude: req.results.lng
-    };
-  }
- 
-  /* Save the listing */
-  listing.save(function(err) {
-    if(err) {
-      console.log(err);
-      res.status(400).send(err);
-    } else {
-      res.json(listing);
-      console.log(listing)
+  Listing.findById(req.params.id, function (err, listing) {
+
+    //Check for an error
+    if (err)
+      res.send(err);
+
+    //Update the contact information
+    listing.name = req.body.name;
+    listing.code = req.body.code;
+    listing.address = req.body.address;
+    if (req.results) {
+      listing.coordinates = {
+        latitude: req.results.lat,
+        longitude: req.results.lng
+      }
     }
+
+    listing.save(function (err) {
+      if (err)
+        res.status(400).send(err);
+      res.json({
+        message: 'Updated listing',
+        data: listing
+      });
+    });
   });
 };
+// //DOES NOT WORK 
+// if(!listing.body) {
+//   return res.status(400).send({
+//       message: "Listing does not exist"
+//   }); 
+// }
+// /* Replace the listings's properties with the new properties found in req.body */
+// Listing.findByIdAndUpdate(req.params.id, {
+//   name: req.body.name, 
+//   code: req.body.code    
+// });
+//  /*save the coordinates (located in req.results if there is an address property) */
+// if(req.results) {
+//   listing.coordinates = {
+//     latitude: req.results.lat, 
+//     longitude: req.results.lng
+//   };
+// }
 
+// /* Save the listing */
+// listing.save(function(err) {
+//   if(err) {
+//     console.log(err);
+//     res.status(400).send(err);
+//   } else {
+//     res.json(listing);
+//     console.log(listing)
+//   }
+//});
+//};
+//----------------------------------------------------------------------------------------------
 /* Delete a listing */
-//WORKS
-exports.delete = function(req, res) {
+exports.delete = function (req, res) {
   var listing = req.listing;
-  Listing.collection.findOneAndDelete({code: listing.code, name: listing.name},function(err,res){
+  Listing.collection.findOneAndDelete({ code: listing.code, name: listing.name }, function (err, res) {
     res.status(400).send(err);
-});
+  });
 
 };
+//----------------------------------------------------------------------------------------------
 
 /* Retreive all the directory listings, sorted alphabetically by listing code */
-exports.list = function(req, res, next) {
-  //TODO
-  Listing.find({},function(err, listing) {
-    if(err) {
+exports.list = function (req, res, next) {
+  Listing.find({}, function (err, listings) {
+    if (err) {
       res.status(400).send(err);
-    } else {
-      req.listing = listing;
-      console.log(req.listing);
-      next();
+    }
+    else {
+      //res.status(200).send(listings)
+      //console.log(listings);
+      res.json(listings);
+      res.status(200);
     }
   });
 };
+//----------------------------------------------------------------------------------------------
 
 /* 
   Middleware: find a listing by its ID, then pass it to the next request handler. 
@@ -115,9 +149,10 @@ exports.list = function(req, res, next) {
         bind it to the request object as the property 'listing', 
         then finally call next
  */
-exports.listingByID = function(req, res, next, id) {
-  Listing.findById(id).exec(function(err, listing) {
-    if(err) {
+
+exports.listingByID = function (req, res, next, id) {
+  Listing.findById(id).exec(function (err, listing) {
+    if (err) {
       res.status(400).send(err);
     } else {
       req.listing = listing;
